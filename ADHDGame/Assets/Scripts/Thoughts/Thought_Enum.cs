d8746beaaaa3864dba0679b108908ce8bff5d0aa
Anatnatz <anatzayd@gmail.com>
2023-07-06 20:52:02 +0300

    added objects, thoughts, and tasks - mostely not working

--- ADHDGame/Assets/Scripts/Thoughts/Thought_Enum.cs
+++ ADHDGame/Assets/Scripts/Thoughts/Thought_Enum.cs
@@ -8,7 +8,15 @@ public enum Thought_Enum
     I_have_to_pee,
     COFFEE,
     Gotta_get_to_the_office_at_8_30,
-    This_towel_smells
+    This_towel_smells,
+    Stepping_on_legos,
+    Unload_dishes,
+    Take_lunch,
+    Work_on_game,
+    Water_the_plant,
+    Gotta_GO_poop,
+    Chilly_out,
+    Cant_wash_towel
 
 
 }
