7661fcba4ec6912a4112396038a7f7c99dce5756
Anatnatz <anatzayd@gmail.com>
2023-07-09 12:02:21 +0300

    created on hover script

---
+++ ADHDGame/Assets/Scripts/Mouse/TextOnHover.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextOnHover : MonoBehaviour
+{
+    [SerializeField]
+    public string hoveredText;
+
+    GameObject textObject;
+    void Start()
+    {
+        SetObjectText();
+    }
+
+    void SetObjectText()
+    {
+        TextMesh objTxtComponent = transform.GetComponentInChildren<TextMesh>();
+        textObject = objTxtComponent.gameObject;
+        Debug.Log(objTxtComponent);
+        objTxtComponent.text = hoveredText;
+        textObject.SetActive(false);
+    }
+
+    void OnMouseEnter()
+    {
+        textObject.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        textObject.SetActive(false);
+    }
+}
