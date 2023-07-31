bcdbe378a3fbafeb88fe17cfd312ad17785533a5
Anatnatz <anatzayd@gmail.com>
2023-07-31 16:26:22 +0300

    drag left to ignore

--- ADHDGame/Assets/Scripts/Thoughts/DraggableItem.cs
+++ ADHDGame/Assets/Scripts/Thoughts/DraggableItem.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class
 DraggableItem
-: MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+: MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [HideInInspector]
     public Transform parentAfterDrag;
@@ -40,6 +41,19 @@ DraggableItem
         isDragging = false;
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TMP_Text thoughtTxt = transform.GetChild(0).GetComponent<TMP_Text>();
+        thoughtTxt.SetText("Drag left to ignore");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TMP_Text thoughtTxt = transform.GetChild(0).GetComponent<TMP_Text>();
+        thoughtTxt.SetText(GetComponent<thought_Transform>().thoughtText);
+    }
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("trigger");
@@ -48,4 +62,6 @@ DraggableItem
             thoughtTrigger.TriggerThought(other);
         }
     }
+
+
 }
